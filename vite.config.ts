92f80d112bfd0a29c86c7ff7e9a0345ebbeeed97
Muspi Merol <me@promplate.dev>
2025-07-27 03:56:25 +0800

    remove vite-plugin-wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import UnoCSS from "unocss/vite"
 import { defineConfig } from "vite"
-import wasm from "vite-plugin-wasm"
 
 export default defineConfig({
-	plugins: [sveltekit(), UnoCSS(), wasm()],
+	plugins: [sveltekit(), UnoCSS()],
 	worker: { format: "es" }
 })
